fd6aa720065af0c3a7d8ae652cb1c3d9d0b30843
AngeloZorzi <astro.drg666@gmail.com>
2025-05-07 16:53:19 +0200

    up

--- src/Esercizio2/Main.java
+++ src/Esercizio2/Main.java
@@ -1,4 +1,9 @@
 package Esercizio2;
 
 public class Main {
+    public static void main(String[] args) {
+        SIM sim = new SIM("3201234567");
+        sim.stampaDatiSIM();
+    }
+
 }
--- src/Esercizio2/SIM.java
+++ src/Esercizio2/SIM.java
@@ -1,6 +1,28 @@
 package Esercizio2;
 
-public class SIM {
+import java.util.List;
 
+public class SIM {
+    private String noTelefono;
+    private double credito;
+
+
+    public SIM(String noTelefono){
+        this.noTelefono = noTelefono;
+        this.credito = 0.0;
+
+    }
+
+    public String getNoTelefono(){
+        return noTelefono;
+    }
+    public double getCredito(){
+        return credito;
+    }
+
+    public void stampaDatiSIM(){
+        System.out.println("Numero SIM: " + noTelefono);
+        System.out.println("Credito disponibile: €" + credito);
+    }
 
 }
